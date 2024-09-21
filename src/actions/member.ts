4a78cbe54f42dc333d94bffa59684d426bc01263
AuJung <nuttapong6280@gmail.com>
2024-09-21 14:11:00 +0700

    feat(recentMember): error handling & fetching data

--- src/actions/member.ts
+++ src/actions/member.ts
@@ -2,14 +2,20 @@
 
 import axios from '@/lib/axios'
 import { RecentMember } from '@/types/member'
+import { actionResponse } from '@/types/reponse'
 
-export const getRecentMembers = async (limit: number) => {
+export const getRecentMembers = async (
+  limit: number,
+): Promise<actionResponse<RecentMember>> => {
   try {
     const { data } = await axios.get<RecentMember>(
       `/member/recent?limit=${limit}`,
     )
-    return data
+    return { data, error: null }
   } catch (e) {
-    return []
+    return {
+      data: [],
+      error: 'Failed to fetch recent members. Please try again.',
+    }
   }
 }
--- src/app/(auth)/member/@recentMember/page.tsx
+++ src/app/(auth)/member/@recentMember/page.tsx
@@ -18,8 +18,15 @@ import {
 import ToolTip from '@/components/ToolTip'
 import dayjs from 'dayjs'
 import { getRecentMembers } from '@/actions/member'
+import { revalidateTag } from 'next/cache'
+
 export default async function RecentMember() {
-  const recentMember = await getRecentMembers(8)
+  const { data: recentMember, error } = await getRecentMembers(8)
+
+  const revalidateRecentMember = async () => {
+    'use server'
+    revalidateTag('@recentMember')
+  }
 
   return (
     <>
@@ -32,35 +39,43 @@ export default async function RecentMember() {
             </CardDescription>
           </div>
           <ToolTip content={'Refresh'} side={'top'}>
-            <Button size="icon" className="ml-auto gap-1" variant={'ghost'}>
-              <RotateCcw className={'w-5 h-5'} />
-            </Button>
+            <form action={revalidateRecentMember} className={'ml-auto gap-1'}>
+              <Button size="icon" type={'submit'} variant={'ghost'}>
+                <RotateCcw className={'w-5 h-5'} />
+              </Button>
+            </form>
           </ToolTip>
         </CardHeader>
         <CardContent>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Member</TableHead>
-                <TableHead className="text-right">Time</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {recentMember.map((member, index) => (
-                <TableRow key={index}>
-                  <TableCell className={''}>
-                    <div className="font-medium">{member.userName}</div>
-                    <div className="hidden text-sm text-muted-foreground md:inline">
-                      {member.email}
-                    </div>
-                  </TableCell>
-                  <TableCell className="text-right">
-                    {dayjs(member.createdAt).format('DD MMM, YYYY')}
-                  </TableCell>
+          {error ? (
+            <div className="text-red-500">{error}</div>
+          ) : (
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Member</TableHead>
+                  <TableHead className="text-right">Registered Time</TableHead>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+              </TableHeader>
+              <TableBody>
+                {recentMember.map((member, index) => (
+                  <TableRow key={index}>
+                    <TableCell className={''}>
+                      <div className="font-medium">
+                        {member.userName} ({member.roleName})
+                      </div>
+                      <div className="hidden text-sm text-muted-foreground md:inline">
+                        {member.email}
+                      </div>
+                    </TableCell>
+                    <TableCell className="text-right">
+                      {dayjs(member.createdAt).format('DD MMM, YYYY')}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          )}
         </CardContent>
       </Card>
     </>
