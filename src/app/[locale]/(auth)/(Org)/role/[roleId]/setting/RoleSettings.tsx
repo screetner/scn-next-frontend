6af88b89bb14a8876afdb7fc2a9b9e63874634cb
AuJung <nuttapong6280@gmail.com>
2024-10-18 15:58:19 +0700

    fix: css overflow

--- src/app/[locale]/(auth)/(Org)/role/[roleId]/setting/RoleSettings.tsx
+++ src/app/[locale]/(auth)/(Org)/role/[roleId]/setting/RoleSettings.tsx
@@ -17,45 +17,47 @@ export default function RoleSettings({
 }: RoleSettingsProps) {
   const { roleManageInfo, roleId } = useRoleSetting()
   return (
-    <Card className="flex-1">
-      <CardHeader>
-        <CardTitle>
-          EDIT ROLE — {roleManageInfo.roleInfo.roleName.toLocaleUpperCase()}
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Tabs
-          defaultValue="display"
-          value={activeTab}
-          onValueChange={setActiveTab}
-          className="w-full"
-        >
-          <TabsList className="mb-4">
-            <TabsTrigger value="display">Display</TabsTrigger>
-            <TabsTrigger value="permissions">Permissions</TabsTrigger>
-            <TabsTrigger value="members">Members</TabsTrigger>
-          </TabsList>
-          <TabsContent value="display">
-            <EditRoleNameForm
-              roleId={roleId}
-              initialRoleName={roleManageInfo.roleInfo.roleName}
-            />
-          </TabsContent>
-          <TabsContent value="permissions">
-            <RolePermissionsForm
-              data={roleManageInfo.rolePermissions}
-              roleId={roleId}
-            />
-          </TabsContent>
-          <TabsContent value="members">
-            <Member
-              roleId={roleId}
-              roleMembers={roleManageInfo.roleMembers}
-              roleName={roleManageInfo.roleInfo.roleName}
-            />
-          </TabsContent>
-        </Tabs>
-      </CardContent>
-    </Card>
+    <div className={'flex-1'}>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            EDIT ROLE — {roleManageInfo.roleInfo.roleName.toLocaleUpperCase()}
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Tabs
+            defaultValue="display"
+            value={activeTab}
+            onValueChange={setActiveTab}
+            className="w-full"
+          >
+            <TabsList className="mb-4">
+              <TabsTrigger value="display">Display</TabsTrigger>
+              <TabsTrigger value="permissions">Permissions</TabsTrigger>
+              <TabsTrigger value="members">Members</TabsTrigger>
+            </TabsList>
+            <TabsContent value="display">
+              <EditRoleNameForm
+                roleId={roleId}
+                initialRoleName={roleManageInfo.roleInfo.roleName}
+              />
+            </TabsContent>
+            <TabsContent value="permissions">
+              <RolePermissionsForm
+                data={roleManageInfo.rolePermissions}
+                roleId={roleId}
+              />
+            </TabsContent>
+            <TabsContent value="members">
+              <Member
+                roleId={roleId}
+                roleMembers={roleManageInfo.roleMembers}
+                roleName={roleManageInfo.roleInfo.roleName}
+              />
+            </TabsContent>
+          </Tabs>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
