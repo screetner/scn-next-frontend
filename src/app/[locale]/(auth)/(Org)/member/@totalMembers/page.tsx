4ed6ca97ae138857fc3f2112ca966384d8463133
AuJung <nuttapong6280@gmail.com>
2024-11-06 12:16:04 +0700

    feat: apply TH for Total member

--- src/app/[locale]/(auth)/(Org)/member/@totalMembers/page.tsx
+++ src/app/[locale]/(auth)/(Org)/member/@totalMembers/page.tsx
@@ -3,16 +3,21 @@ import { Users, TrendingUp, TrendingDown } from 'lucide-react'
 import { TypographyH2 } from '@/components/typography/TypographyH2'
 import dayjs from 'dayjs'
 import { getTotalMembers } from '@/actions/member'
+import { getTranslations } from 'next-intl/server'
 
 export default async function TotalMembersPage() {
   const { data, error } = await getTotalMembers()
+  const t = await getTranslations('MemberPage.totalMember')
+  const t2 = await getTranslations('MemberPage')
 
   const growthPositive = data!.percentageIncrease > 0
 
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between pb-4">
-        <CardTitle className="text-lg font-semibold">Total Members</CardTitle>
+        <CardTitle className="text-lg font-semibold">
+          {t('totalMembersTitle')}
+        </CardTitle>
         <Users className="h-5 w-5 text-muted-foreground" />
       </CardHeader>
       <CardContent>
@@ -21,9 +26,7 @@ export default async function TotalMembersPage() {
         ) : (
           <div className="space-y-2">
             <TypographyH2 text={`+${data!.allMembers}`} />
-            <div className="text-sm text-gray-500">
-              Members in the organization
-            </div>
+            <div className="text-sm text-gray-500">{t('description')}</div>
 
             <div className="flex items-center text-sm mt-2 text-gray-500">
               <span
@@ -34,13 +37,15 @@ export default async function TotalMembersPage() {
                 ) : (
                   <TrendingDown className="w-4 h-4 mr-1" />
                 )}
-                {Math.abs(data!.percentageIncrease)}%{' '}
-                {growthPositive ? 'increase' : 'decrease'} since last month
+                {t('growth', {
+                  percentage: Math.abs(data!.percentageIncrease),
+                  growth: growthPositive ? 'increase' : 'decrease',
+                })}
               </span>
             </div>
 
             <div className="text-xs text-gray-400 mt-2">
-              Last updated: {dayjs().format('DD MMM YYYY')}
+              {t2('lastUpdated')}: {dayjs().format('DD MMM YYYY')}
             </div>
           </div>
         )}
