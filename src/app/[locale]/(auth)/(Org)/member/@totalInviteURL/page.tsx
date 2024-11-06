38d891ca0decd611ffeab3c6ba17a5b23ec7666d
AuJung <nuttapong6280@gmail.com>
2024-11-06 12:41:42 +0700

    feat: apply TH for Total invite

--- src/app/[locale]/(auth)/(Org)/member/@totalInviteURL/page.tsx
+++ src/app/[locale]/(auth)/(Org)/member/@totalInviteURL/page.tsx
@@ -2,15 +2,18 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Link } from 'lucide-react'
 import { getTotalInvitees } from '@/actions/member'
 import { TypographyH2 } from '@/components/typography/TypographyH2'
+import { getTranslations } from 'next-intl/server'
 
 export default async function TotalInviteURLPage() {
   const { data, error } = await getTotalInvitees()
+  const t = await getTranslations('MemberPage.totalInviteURL')
+  const t2 = await getTranslations('MemberPage')
 
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between pb-4">
         <CardTitle className="text-lg font-semibold">
-          Invite URL Stats
+          {t('totalInviteURLTitle')}
         </CardTitle>
         <Link className="h-5 w-5 text-muted-foreground" />
       </CardHeader>
@@ -22,7 +25,7 @@ export default async function TotalInviteURLPage() {
             <TypographyH2 text={`${data?.inviteTotal} Total Invites`} />
             <div className="flex items-center text-xl">
               <span className="font-semibold text-green-600">
-                {data?.inviteActivate} Activated
+                {data?.inviteActivate} {t('active')}
               </span>
               <span className="ml-2 text-gray-500 text-sm">
                 {data!.inviteTotal > 0
@@ -34,7 +37,7 @@ export default async function TotalInviteURLPage() {
             <div className="text-sm text-gray-500 mt-2">
               {data!.inviteTotal > 0 && (
                 <span>
-                  Last updated:{' '}
+                  {t2('lastUpdated')}:{' '}
                   <strong>{new Date().toLocaleDateString()}</strong>
                 </span>
               )}
