3d4eee77abed6d66e123544514d274f7c9d22080
AuJung <nuttapong6280@gmail.com>
2024-11-10 12:36:41 +0700

    chore: apply datadog dashboard

--- src/app/[locale]/(auth)/(Owner)/owner/page.tsx
+++ src/app/[locale]/(auth)/(Owner)/owner/page.tsx
@@ -1,3 +1,11 @@
 export default function MainAdminPage() {
-  return <div>Admin Page</div>
+  return (
+    <iframe
+      src="https://p.us5.datadoghq.com/sb/5a2fa78b-9817-11ef-83c9-76a653e73225-20ff214a8cedd380a0395d57a41c8ebe"
+      title="Datadog Dashboard"
+      width="100%"
+      height="100%"
+      loading="eager"
+    ></iframe>
+  )
 }
